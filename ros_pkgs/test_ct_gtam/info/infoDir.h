638474175b1fb7e50889d508d4e998e5cb9b3362
Shuo <paulyang1990@gmail.com>
2020-11-03 22:53:28 -0500

    use control toolbox to do iLQR

---
+++ ros_pkgs/test_ct_gtam/info/infoDir.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace ct {
+namespace optcon {
+
+// the following path will be set automatically during building (do not copy this file manually!)
+static const std::string infoDir = "/root/catkin_ws/src/link_ros_pkgs/test_ct_gtam/info";
+
+}
+}
